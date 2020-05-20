40f014fa7616854054f9a093870d749813e5d742
maximgeller <maxim.geller@yahoo.com>
2020-05-20 13:53:02 -0400

    last step is styling

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,22 +1,19 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  );
-  console.log(request.selection);
-
+  // console.log("Request");
+  // console.log(request.empty);
   let clippings = [];
   chrome.storage.sync.get("list", function (result) {
-    console.log(result);
-    if (request.selection && result.list) {
+    // console.log(result);
+    if (request.empty == "clear") {
+      clippings = [];
+    } else if (request.selection && result.list) {
       clippings = [...result.list, request.selection];
     } else if (result.list) {
       clippings = [...result.list];
     } else {
       clippings = [request.selection];
     }
-    console.log(clippings);
+    // console.log(clippings);
     sendResponse({ clips: clippings });
     chrome.storage.sync.set({
       list: clippings,
@@ -24,27 +21,4 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   });
   return true;
 });
-
-console.log("Stuff");
-
-function addToNotes() {
-  console.log("We here");
-  chrome.tabs.executeScript(
-    {
-      code: "window.getSelection().toString();",
-    },
-    function (selection) {
-      console.log("hi");
-      document.getElementById("output").innerHTML = selection[0];
-      console.log(selection[0]);
-      console.log(document.getElementById("output").innerHTML);
-      chrome.storage.sync.set("list", function (getEvent) {
-        let clippings = [selection[0]];
-        chrome.storage.sync.set({
-          list: clippings,
-        });
-      });
-    }
-  );
-}
 
