e3beb6baf59d8f70857dd7e9d3d87d3b2f4f5f75
maximgeller <maxim.geller@yahoo.com>
2020-05-25 18:09:26 -0400

    begin style

--- web-app-flask/main.py
+++ web-app-flask/main.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") # default
 def home():
-    return "hello world!"
+    return render_template("home.html")
 
-@app.route("/enlight")
-def enlight():
-    return "Hey enlight! Fancy seeing you here!"
-
+@app.route("/cal")
+def cal():
+    return render_template("cal.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
